e225e19a772eca5f1634c3f8670d11229ebcfd07
Matvii Oleh <oleh.matvii@bitsorchestra.com>
2024-11-09 14:05:22 +0200

    feat: init

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+export default {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com'
+      },
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com'
+      }
+    ]
+  }
 };
-
-export default nextConfig;
