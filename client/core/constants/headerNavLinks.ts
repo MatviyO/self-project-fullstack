754e9c4c09b3eda92d1cea19de34afa19997008b
Matvii Oleh <oleh.m97@gmail.com>
2024-11-15 23:15:56 +0200

    feat: new connection

---
+++ client/core/constants/headerNavLinks.ts
@@ -0,0 +1,10 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/contact', title: 'Contact' },
+  { href: '/about', title: 'About' },
+  { href: '/login', title: 'Login' },
+]
+
+export default headerNavLinks
